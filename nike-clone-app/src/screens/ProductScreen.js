5aee4d8de5439c9d71833f6d5684bb07643ed4d6
arthurchina92 <arthurchinaa@gmail.com>
2023-06-26 20:46:26 -0600

    ProductScreen

---
+++ nike-clone-app/src/screens/ProductScreen.js
@@ -0,0 +1,36 @@
+import { View, Text, StyleSheet, FlatList, Image } from "react-native";
+import React from "react";
+import products from "../data/products";
+
+const ProductScreen = () => {
+  return (
+    <FlatList
+      data={products}
+      renderItem={({ item }) => (
+        <View style={styles.itemContainer}>
+          <Image source={{ uri: item.image }} style={styles.image} />
+        </View>
+      )}
+      numColumns={2}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  itemContainer: {
+    width: "50%",
+    padding: 1,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1 / 1,
+  },
+});
+
+export default ProductScreen;
